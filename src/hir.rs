3947534a81f4c61c2078e044705d583d8a93a90a
Garnet3106 <manage@gant.work>
2023-01-03 02:56:36 +0900

    Add data type hirizer

--- src/hir.rs
+++ src/hir.rs
@@ -45,7 +45,7 @@ impl<'a> HirGenerator<'a> {
         Ok((hir, self.logs))
     }
 
-    pub(crate) fn item(&mut self, node: &SyntaxNode) -> HirGeneratorResult<HirItem> {
+    pub fn item(&mut self, node: &SyntaxNode) -> HirGeneratorResult<HirItem> {
         let child_node = node.child_node_at(0);
 
         let item = match child_node.name.as_str() {
@@ -56,7 +56,7 @@ impl<'a> HirGenerator<'a> {
         Ok(item)
     }
 
-    pub(crate) fn module(&mut self, node: &SyntaxNode) -> HirGeneratorResult<HirModule> {
+    pub fn module(&mut self, node: &SyntaxNode) -> HirGeneratorResult<HirModule> {
         let identifier_node  = node.search_node("Identifier::identifier").unwrap();
         let identifier = self.identifier(identifier_node, HirIdentifierKind::PascalCase);
         let visibility = HirVisibility::Private;
@@ -75,7 +75,7 @@ impl<'a> HirGenerator<'a> {
         Ok(module)
     }
 
-    pub(crate) fn identifier(&mut self, node: &SyntaxNode, valid_identifier_kind: HirIdentifierKind) -> String {
+    pub fn identifier(&mut self, node: &SyntaxNode, valid_identifier_kind: HirIdentifierKind) -> String {
         let child_node = node.child_node_at(0);
 
         let identifier_kind = match child_node.name.as_str() {
@@ -92,6 +92,38 @@ impl<'a> HirGenerator<'a> {
         }
 
         identifier.to_string()
+    }
+
+    pub fn data_type(&mut self, node: &SyntaxNode) -> HirDataType {
+        let child_node = node.child_node_at(0);
+
+        match child_node.name.as_str() {
+            "DataType::primitive" => {
+                let value = &child_node.child_leaf_at(0).value;
+
+                let primitive = match value.as_str() {
+                    "bool" => HirPrimitiveDataType::Bool,
+                    "s8" => HirPrimitiveDataType::S8,
+                    "s16" => HirPrimitiveDataType::S16,
+                    "s32" => HirPrimitiveDataType::S32,
+                    "s64" => HirPrimitiveDataType::S64,
+                    "ssize" => HirPrimitiveDataType::SSize,
+                    "u8" => HirPrimitiveDataType::U8,
+                    "u16" => HirPrimitiveDataType::U16,
+                    "u32" => HirPrimitiveDataType::U32,
+                    "u64" => HirPrimitiveDataType::U64,
+                    "usize" => HirPrimitiveDataType::USize,
+                    "f32" => HirPrimitiveDataType::F32,
+                    "f64" => HirPrimitiveDataType::F64,
+                    "char" => HirPrimitiveDataType::Character,
+                    "str" => HirPrimitiveDataType::String,
+                    _ => unreachable!(),
+                };
+
+                HirDataType::Primitive(primitive)
+            },
+            _ => unreachable!(),
+        }
     }
 }
 
@@ -116,4 +148,29 @@ pub struct HirModule {
     pub identifier: String,
     pub visibility: HirVisibility,
     pub items: Vec<HirItem>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum HirDataType {
+    Primitive(HirPrimitiveDataType),
+    Named(String),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum HirPrimitiveDataType {
+    Bool,
+    S8,
+    S16,
+    S32,
+    S64,
+    SSize,
+    U8,
+    U16,
+    U32,
+    U64,
+    USize,
+    F32,
+    F64,
+    Character,
+    String,
 }
--- src/tests/hir.rs
+++ src/tests/hir.rs
@@ -2,38 +2,53 @@ use crate::{hir::*, tests::generator::*};
 use speculate::speculate;
 
 speculate!{
-    it "generate module" {
-        let options = HirGeneratorOptions {};
-        let mut generator = HirGenerator::new(&options);
+    before {
+        let default_hir_options = HirGeneratorOptions {};
+    }
 
-        let left = generator.module(
-            &SyntaxElementGenerator::module(
-                "private",
-                SyntaxElementGenerator::identifier(HirIdentifierKind::PascalCase, "Module"),
-                vec![
-                    SyntaxElementGenerator::item(
-                        SyntaxElementGenerator::module(
-                            "private",
-                            SyntaxElementGenerator::identifier(HirIdentifierKind::PascalCase, "SubModule"),
-                            vec![],
+    it "hirize module" {
+        let mut generator = HirGenerator::new(&default_hir_options);
+
+        assert_eq!(
+            generator.module(
+                &SyntaxElementGenerator::module(
+                    "private",
+                    SyntaxElementGenerator::identifier(HirIdentifierKind::PascalCase, "Module"),
+                    vec![
+                        SyntaxElementGenerator::item(
+                            SyntaxElementGenerator::module(
+                                "private",
+                                SyntaxElementGenerator::identifier(HirIdentifierKind::PascalCase, "SubModule"),
+                                vec![],
+                            ),
                         ),
-                    ),
+                    ],
+                ).into_node(),
+            ).unwrap(),
+            HirModule {
+                identifier: "Module".to_string(),
+                visibility: HirVisibility::Private,
+                items: vec![
+                    HirItem::Module(HirModule {
+                        identifier: "SubModule".to_string(),
+                        visibility: HirVisibility::Private,
+                        items: vec![],
+                    }),
                 ],
-            ).into_node(),
-        ).unwrap();
+            }
+        );
+    }
 
-        let right = HirModule {
-            identifier: "Module".to_string(),
-            visibility: HirVisibility::Private,
-            items: vec![
-                HirItem::Module(HirModule {
-                    identifier: "SubModule".to_string(),
-                    visibility: HirVisibility::Private,
-                    items: vec![],
-                }),
-            ],
-        };
+    it "hirize data type" {
+        let mut generator = HirGenerator::new(&default_hir_options);
 
-        assert_eq!(left, right);
+        assert_eq!(
+            generator.data_type(
+                &SyntaxElementGenerator::data_type(
+                    SyntaxElementGenerator::primitive_data_type("bool"),
+                ).into_node(),
+            ),
+            HirDataType::Primitive(HirPrimitiveDataType::Bool),
+        );
     }
 }
