31fef8332d8bba7f022b5ea491011b4013d97e88
Garnet3106 <manage@gant.work>
2023-01-03 23:12:23 +0900

    Fix literal tests to expressions

--- src/tests/syntax.rs
+++ src/tests/syntax.rs
@@ -51,145 +51,175 @@ speculate!{
 
     /* DataType */
 
-    it "parse primitive type" {
-        expect_success("s32", "DataType::data_type", tree!{
-            SyntaxElementGenerator::data_type(
-                SyntaxElementGenerator::primitive_data_type("s32"),
+    it "parse primitive type expression" {
+        expect_success("s32", "Expression::expression", tree!{
+            SyntaxElementGenerator::expression(
+                SyntaxElementGenerator::data_type(
+                    SyntaxElementGenerator::primitive_data_type("s32"),
+                ),
             )
         });
     }
 
     /* Literal */
 
-    it "parse boolean literal" {
-        expect_success("true", "Literal::literal", tree!{
-            SyntaxElementGenerator::boolean_literal("true")
-        });
-    }
-
-    it "parse integer literal" {
-        expect_success("000", "Literal::literal", tree!{
-            SyntaxElementGenerator::integer_literal("dec", "000", None)
-        });
-    }
-
-    it "parse bin integer literal" {
-        expect_success("0b0_111", "Literal::literal", tree!{
-            SyntaxElementGenerator::integer_literal("bin", "0111", None)
-        });
-    }
-
-    it "parse oct integer literal" {
-        expect_success("0o01_234_567", "Literal::literal", tree!{
-            SyntaxElementGenerator::integer_literal("oct", "01234567", None)
-        });
-    }
-
-    it "parse hex integer literal" {
-        expect_success("0x0_123_456_789_abc_def", "Literal::literal", tree!{
-            SyntaxElementGenerator::integer_literal("hex", "0123456789abcdef", None)
-        });
-    }
-
-    it "parse integer literal with integer type suffix" {
-        expect_success("000s32", "Literal::literal", tree!{
-            SyntaxElementGenerator::integer_literal("dec", "000", Some("s32"))
-        });
-    }
-
-    it "parse integer literal with float type suffix" {
-        expect_failure("0f32", "Literal::literal", ParserError::ExpectedEndOfInput);
-    }
-
-    it "parse number chain without separator" {
-        expect_success("1000", "Literal::literal", tree!{
-            SyntaxElementGenerator::integer_literal("dec", "1000", None)
-        });
-    }
-
-    it "parse number chain with separators" {
-        expect_success("10_000_000", "Literal::literal", tree!{
-            SyntaxElementGenerator::integer_literal("dec", "10000000", None)
-        });
-    }
-
-    it "parse float literal" {
-        expect_success("000.000", "Literal::literal", tree!{
-            SyntaxElementGenerator::float_literal("000", "000", None)
-        });
-    }
-
-    it "parse float literal with and without separator" {
-        expect_failure("000.000_0", "Literal::literal", ParserError::ExpectedEndOfInput);
-    }
-
-    it "parse float literal with float type suffix" {
-        expect_success("000.000f32", "Literal::literal", tree!{
-            SyntaxElementGenerator::float_literal("000", "000", Some("f32"))
-        });
-    }
-
-    it "parse float literal with integer type suffix" {
-        expect_failure("000.000s32", "Literal::literal", ParserError::ExpectedEndOfInput);
-    }
-
-    it "parse character literal" {
-        expect_success("'a'", "Literal::literal", tree!{
-            SyntaxElementGenerator::character_literal(
-                SyntaxElementGenerator::single_character(leaf!("a")),
+    it "parse boolean literal expression" {
+        expect_success("true", "Expression::expression", tree!{
+            SyntaxElementGenerator::expression(
+                SyntaxElementGenerator::boolean_literal("true"),
             )
         });
     }
 
-    it "parse character literal including escape sequence" {
-        expect_success("'\\u{0}'", "Literal::literal", tree!{
-            SyntaxElementGenerator::character_literal(
-                SyntaxElementGenerator::single_character(
-                    SyntaxElementGenerator::unicode_escape_sequence("0"),
+    it "parse integer literal expression" {
+        expect_success("000", "Expression::expression", tree!{
+            SyntaxElementGenerator::expression(
+                SyntaxElementGenerator::integer_literal("dec", "000", None),
+            )
+        });
+    }
+
+    it "parse bin integer literal expression" {
+        expect_success("0b0_111", "Expression::expression", tree!{
+            SyntaxElementGenerator::expression(
+                SyntaxElementGenerator::integer_literal("bin", "0111", None),
+            )
+        });
+    }
+
+    it "parse oct integer literal expression" {
+        expect_success("0o01_234_567", "Expression::expression", tree!{
+            SyntaxElementGenerator::expression(
+                SyntaxElementGenerator::integer_literal("oct", "01234567", None),
+            )
+        });
+    }
+
+    it "parse hex integer literal expression" {
+        expect_success("0x0_123_456_789_abc_def", "Expression::expression", tree!{
+            SyntaxElementGenerator::expression(
+                SyntaxElementGenerator::integer_literal("hex", "0123456789abcdef", None),
+            )
+        });
+    }
+
+    it "parse integer literal expression with integer type suffix" {
+        expect_success("000s32", "Expression::expression", tree!{
+            SyntaxElementGenerator::expression(
+                SyntaxElementGenerator::integer_literal("dec", "000", Some("s32")),
+            )
+        });
+    }
+
+    it "parse integer literal expression with float type suffix" {
+        expect_failure("0f32", "Expression::expression", ParserError::ExpectedEndOfInput);
+    }
+
+    it "parse number chain expression without separator" {
+        expect_success("1000", "Expression::expression", tree!{
+            SyntaxElementGenerator::expression(
+                SyntaxElementGenerator::integer_literal("dec", "1000", None),
+            )
+        });
+    }
+
+    it "parse number chain expression with separators" {
+        expect_success("10_000_000", "Expression::expression", tree!{
+            SyntaxElementGenerator::expression(
+                SyntaxElementGenerator::integer_literal("dec", "10000000", None),
+            )
+        });
+    }
+
+    it "parse float literal expression" {
+        expect_success("000.000", "Expression::expression", tree!{
+            SyntaxElementGenerator::expression(
+                SyntaxElementGenerator::float_literal("000", "000", None),
+            )
+        });
+    }
+
+    it "parse float literal expression with and without separator" {
+        expect_failure("000.000_0", "Expression::expression", ParserError::ExpectedEndOfInput);
+    }
+
+    it "parse float literal expression with float type suffix" {
+        expect_success("000.000f32", "Expression::expression", tree!{
+            SyntaxElementGenerator::expression(
+                SyntaxElementGenerator::float_literal("000", "000", Some("f32")),
+            )
+        });
+    }
+
+    it "parse float literal expression with integer type suffix" {
+        expect_failure("000.000s32", "Expression::expression", ParserError::ExpectedEndOfInput);
+    }
+
+    it "parse character literal expression" {
+        expect_success("'a'", "Expression::expression", tree!{
+            SyntaxElementGenerator::expression(
+                SyntaxElementGenerator::character_literal(
+                    SyntaxElementGenerator::single_character(leaf!("a")),
                 ),
             )
         });
     }
 
-    it "parse character literal including newline" {
-        expect_failure("'\n'", "Literal::literal", ParserError::UnexpectedEndOfInput);
-    }
-
-    it "parse character literal including single quotation" {
-        expect_failure("'''", "Literal::literal", ParserError::UnexpectedEndOfInput);
-    }
-
-    it "parse character literal including double quotation escape" {
-        expect_failure("'\\\"'", "Literal::literal", ParserError::UnexpectedEndOfInput);
-    }
-
-    it "parse empty string literal" {
-        expect_success("\"\"", "Literal::literal", tree!{
-            SyntaxElementGenerator::string_literal(vec![])
+    it "parse character literal expression including escape sequence" {
+        expect_success("'\\u{0}'", "Expression::expression", tree!{
+            SyntaxElementGenerator::expression(
+                SyntaxElementGenerator::character_literal(
+                    SyntaxElementGenerator::single_character(
+                        SyntaxElementGenerator::unicode_escape_sequence("0"),
+                    ),
+                )
+            )
         });
     }
 
-    it "parse string literal" {
-        expect_success("\"a\\n\"", "Literal::literal", tree!{
-            SyntaxElementGenerator::string_literal(vec![
-                SyntaxElementGenerator::single_character(leaf!("a")),
-                SyntaxElementGenerator::single_character(
-                    SyntaxElementGenerator::general_escape_sequence("n"),
-                ),
-            ])
+    it "parse character literal expression including newline" {
+        expect_failure("'\n'", "Expression::expression", ParserError::UnexpectedEndOfInput);
+    }
+
+    it "parse character literal expression including single quotation" {
+        expect_failure("'''", "Expression::expression", ParserError::UnexpectedEndOfInput);
+    }
+
+    it "parse character literal expression including double quotation escape" {
+        expect_failure("'\\\"'", "Expression::expression", ParserError::UnexpectedEndOfInput);
+    }
+
+    it "parse empty string literal expression" {
+        expect_success("\"\"", "Expression::expression", tree!{
+            SyntaxElementGenerator::expression(
+                SyntaxElementGenerator::string_literal(vec![]),
+            )
         });
     }
 
-    it "parse string literal including newline" {
-        expect_failure("\"\n\"", "Literal::literal", ParserError::UnexpectedEndOfInput);
+    it "parse string literal expression" {
+        expect_success("\"a\\n\"", "Expression::expression", tree!{
+            SyntaxElementGenerator::expression(
+                SyntaxElementGenerator::string_literal(vec![
+                    SyntaxElementGenerator::single_character(leaf!("a")),
+                    SyntaxElementGenerator::single_character(
+                        SyntaxElementGenerator::general_escape_sequence("n"),
+                    ),
+                ]),
+            )
+        });
     }
 
-    it "parse string literal including double quotation" {
-        expect_failure("\"\"\"", "Literal::literal", ParserError::ExpectedEndOfInput);
+    it "parse string literal expression including newline" {
+        expect_failure("\"\n\"", "Expression::expression", ParserError::UnexpectedEndOfInput);
     }
 
-    it "parse string literal including single quotation escape" {
-        expect_failure("\"\\'\"", "Literal::literal", ParserError::UnexpectedEndOfInput);
+    it "parse string literal expression including double quotation" {
+        expect_failure("\"\"\"", "Expression::expression", ParserError::ExpectedEndOfInput);
+    }
+
+    it "parse string literal expression including single quotation escape" {
+        expect_failure("\"\\'\"", "Expression::expression", ParserError::UnexpectedEndOfInput);
     }
 
     it "parse general escape sequence" {
